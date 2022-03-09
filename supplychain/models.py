01e636e1513da58b75b448c7726a49b59094f909
Saksham <Saksham.khanal01@gmail.com>
2022-03-09 13:46:42 +0545

    Branch with working code

--- supplychain/models.py
+++ supplychain/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from pyexpat import model
 import re
 from tabnanny import verbose
@@ -16,22 +17,24 @@ class User(AbstractUser):
 class Dealer(models.Model):
   
     name = models.CharField(max_length=100,null=True)
-    print('here1')
-
+   
     pan_no =  models.CharField(max_length=15,null=True,verbose_name='PAN number')
-    print('here2')
+
 
     email = models.CharField(max_length=200,null=True,verbose_name='Email')
-    print('here3')
+    
 
     phn =  models.CharField(max_length=10,null=True,verbose_name='Phone')
-    print('here4')
+    
 
     dsp = models.TextField(max_length=100,null=True,verbose_name='Description')
-    print('here5')
+    
 
     crt_usr = models.OneToOneField(User,on_delete=models.CASCADE,verbose_name='Create user',null=True)
-    print('here6')
+    
+    def __str__(self):
+        return self.name
+    
 
     class Meta:
         verbose_name_plural = 'Dealer'
@@ -41,23 +44,26 @@ class Dealer(models.Model):
     
 class Salesman(models.Model):
     name = models.CharField(max_length=100,null=True,verbose_name='Name')
-    print('here7')
+    
 
     total_order=models.CharField(max_length=100,null=True,verbose_name='Total order')
-    print('here8')
+
 
     total_sales =models.CharField(max_length=100,null=True,verbose_name='Total sales')
-    print('here9')
+    
 
     leftover = models.CharField(max_length=100,null=True,verbose_name='Remaining order')
-    print('here10')
+    
 
 
-    saso_dea= models.ForeignKey(Dealer,on_delete=models.CASCADE,related_name='sales_asso_dealer',null=True) #saso_dea = salesman association with dealer
-    print('here11')
+    saso_dea= models.ForeignKey(Dealer,on_delete=models.CASCADE,related_name='sales_asso_dealer',null=True,verbose_name='salesman association with dealer') #saso_dea = salesman association with dealer
 
-    saso_usr = models.OneToOneField(User,on_delete=models.CASCADE,related_name='sales_asso_user',null=True) #saso_usr = salesman association with user
-    print('here12')
+
+    saso_usr = models.OneToOneField(User,on_delete=models.CASCADE,related_name='sales_asso_user',null=True,verbose_name='salesman association with user') #saso_usr = salesman association with user
+
+    def __str__(self):
+        return self.name
+    
 
     class Meta:
         verbose_name_plural = 'Salesman'
@@ -65,41 +71,48 @@ class Salesman(models.Model):
 
 class Items(models.Model):
     name= models.CharField(max_length=100,null=True,verbose_name='Name')
-    print('here13')
+    
 
     price = models.CharField(max_length=100,null=True,verbose_name='Price')
-    print('here14')
+    
 
     dsp = models.CharField(max_length=250,null=True,verbose_name='Description')
-    print('here15')
+    
 
     stk_att =[('In stock','In stock'),('Out of stock','Out of stock')] #stk_att = stock attribute
-    print('here16')
+   
 
     status = models.CharField(max_length=12,choices=stk_att,default='In stock',null=True)
-    print('here17')
+    
 
-    itm_dea = models.ForeignKey(Dealer,on_delete=models.CASCADE,related_name='item_asso_dealer',null=True) #item association with Dealer  
-    print('here18')
+    itm_dea = models.ForeignKey(Dealer,on_delete=models.CASCADE,related_name='item_asso_dealer',null=True,verbose_name='Item association with Dealer') #item association with Dealer  
+
+    def __str__(self):
+        return self.name
+   
 
     class Meta:
         verbose_name_plural = 'Item'
 
 class Order(models.Model):
     Name = models.CharField(max_length=100,null=True)
-    print('here19')
+   
 
     qty = {('box','box'),('kilo','kilo')}
-    print('here20')
+    
 
     quanti = models.CharField(max_length=4,null=True,choices=qty,verbose_name='Quantity')
-    print('here21')
 
-    oaso_sal = models.ForeignKey(Salesman,on_delete=models.CASCADE,related_name='order_asso_sales',null=True)#Order association with sales
-    print('here22')
+    date_crt = models.DateTimeField(auto_now_add=True,verbose_name="Date created")
 
-    iaso_sal = models.ForeignKey(Items,on_delete=models.CASCADE,related_name='items_asso_sales',null=True)#Item association with sales
-    print('here23')
+    oaso_sal = models.ForeignKey(Salesman,on_delete=models.CASCADE,related_name='order_asso_sales',null=True,verbose_name='Order association with sales')#Order association with sales
+    
+
+    iaso_sal = models.ForeignKey(Items,on_delete=models.CASCADE,related_name='items_asso_sales',null=True,verbose_name='Item association with sales')#Item association with sales
+
+    def __str__(self):
+        return self.Name
+    
 
     class Meta:
         verbose_name_plural = 'Order'
