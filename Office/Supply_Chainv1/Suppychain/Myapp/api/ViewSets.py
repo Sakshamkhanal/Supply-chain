796ab4871409d9869a5cc2811d79c86ed2c29b57
Saksham <Saksham.khanal01@gmail.com>
2022-03-20 16:44:14 +0545

    The working API

--- Office/Supply_Chainv1/Suppychain/Myapp/api/ViewSets.py
+++ Office/Supply_Chainv1/Suppychain/Myapp/api/ViewSets.py
@@ -1,79 +1,127 @@
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from html5lib import serialize
-from Myapp.api.serializers import DealerSerializer,SalesmanSerilizer,ItemSerilizer
+from Myapp.api.serializers import DealerSerializer,SalesmanSerilizer,ItemSerilizer,OrderSerializer
 from Myapp.models import Dealer,Item, Order,Salesman
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+
 class DealerViewSet(ModelViewSet):
-    def list(self,request,format=None):
-        dealer =Dealer.objects.all()
-        serializer = DealerViewSet(dealer)
-        return Response(serializer.data)
-    
-    def create(self,request,format=None):
-        serializer = DealerSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def destroy(self,request,pk,format=None):
-        dealer = get_object_or_404(Dealer.objects.all(),pk=pk)
-        dealer.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    permission_classes = []
+    serializer_class = DealerSerializer
+    http_method_names = ['get', 'post', 'put',
+                         'patch', 'head', 'options', 'trace']
+
+    def get_queryset(self):
+        # return today's list for shop of logged in user by default. filter by date if date parameter is provided.
+        queryset = Dealer.objects.all()
+        return queryset
+
+
+    # def perform_create(self, serializer, *args, **kwargs):
+    #     serializer.save(user=self.request.user,
+    #                     shop=self.request.user.user_shop.shop)
+
+
 
 class SalesmanViewSet(ModelViewSet):
-    def list(self,request,format=None):
-        salesman = Salesman.objects.all()
-        serializer = SalesmanViewSet(salesman)
-        return Response(serializer.data)
-    
-    def create(self,request,format=None):
-        serializer = SalesmanSerilizer(request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = SalesmanSerilizer
+    http_method_names = ['get', 'post', 'put',
+                         'patch', 'delete', 'head', 'options', 'trace']
 
-    def destroy(self,request,pk,format=None):
-        salesman = get_object_or_404(Salesman.objects.all(),pk=pk)
-        salesman.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+    def get_queryset(self):
+        query_set = Salesman.objects.all()
+        return query_set
 class ItemsViewSet(ModelViewSet):
-    def list(self,request,format=None):
-        item = Item.objects.all()
-        serializer = ItemsViewSet(request.data)
-        return Response(serializer.data)
+    serializer_class = ItemSerilizer
+    http_method_names = ['get', 'post', 'put',
+                         'patch', 'delete', 'head', 'options', 'trace']
 
-    def create(self,request,format=None):
-        serializer = ItemsViewSet(request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
- 
-    def destroy(self,request,pk,format=None):
-        item = get_object_or_404(Item.objects.all(),pk=pk)
-        item.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+    def get_queryset(self):
+        query_set =  Item.objects.all()
+        return query_set
 class OrderViewset(ModelViewSet):
-    def list(self,request,format=None):
-        order = Order.objects.all()
-        serializer = OrderViewset(request.data)
-        return Response(serializer.data)
+    serializer_class = OrderSerializer
+    http_method_names = ['get', 'post', 'put',
+                         'patch', 'delete', 'head', 'options', 'trace']
 
-    def create(self,request,format=None):
-        serializer = OrderViewset(request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status= status.HTTP_201_CREATED)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        query_set = Order.objects.all()
+        return query_set
+
+
+
+
+################################################################################
+################################################################################
+# class SalesmanViewSet1(ModelViewSet):
+#     def list(self,request,format=None):
+#         salesman = Salesman.objects.all()
+#         serializer = SalesmanViewSet(salesman)
+#         return Response(serializer.data)
     
-    def destroy(self,request,pk,format=None):
-        order = Order.objects.all()
-        serializer = OrderViewset(request.data)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def create(self,request,format=None):
+#         serializer = SalesmanSerilizer(request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+#     def destroy(self,request,pk,format=None):
+#         salesman = get_object_or_404(Salesman.objects.all(),pk=pk)
+#         salesman.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+# ##################################################################################
+# class ItemsViewSet1(ModelViewSet):
+#     def list(self,request,format=None):
+#         item = Item.objects.all()
+#         serializer = ItemsViewSet(request.data)
+#         return Response(serializer.data)
+
+#     def create(self,request,format=None):
+#         serializer = ItemsViewSet(request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#         return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+ 
+#     def destroy(self,request,pk,format=None):
+#         item = get_object_or_404(Item.objects.all(),pk=pk)
+#         item.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# class OrderViewset(ModelViewSet):
+#     def list(self,request,format=None):
+#         order = Order.objects.all()
+#         serializer = OrderViewset(request.data)
+#         return Response(serializer.data)
+
+#     def create(self,request,format=None):
+#         serializer = OrderViewset(request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status= status.HTTP_201_CREATED)
+#         return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+    
+#     def destroy(self,request,pk,format=None):
+#         order = Order.objects.all()
+#         serializer = OrderViewset(request.data)
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+# class DealerViewSet1(ModelViewSet):
+#     def list(self,request,format=None):
+#         dealer = Dealer.objects.all()
+#         serializer = DealerSerializer
+#         return Response(serializer.data)
+    
+#     def create(self,request,format=None):
+#         serializer = DealerSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+#     def destroy(self,request,pk,format=None):
+#         dealer = get_object_or_404(Dealer.objects.all(),pk=pk)
+#         dealer.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
--- Office/Supply_Chainv1/Suppychain/Myapp/models.py
+++ Office/Supply_Chainv1/Suppychain/Myapp/models.py
@@ -6,37 +6,41 @@ class User(AbstractUser):
     pass
 
 class Dealer(models.Model):
-    name = models.CharField(max_length=100,verbose_name='Name')
-    company_name = models.CharField(max_length=100,verbose_name="Company's name")
-    phone = models.CharField(max_length=15,verbose_name='Phone Number',null=True)
+    name = models.CharField(max_length=100,verbose_name='Name',null=True,blank=True)
+    company_name = models.CharField(max_length=100,verbose_name="Company's name",null=True,blank=True)
+    phone = models.CharField(max_length=15,verbose_name='Phone Number',null=True,blank=True)
     additional_contact_info = models.CharField(max_length=100,null=True)
-    address = models.CharField(max_length=100,null=True,verbose_name='Address')
-    description = models.TextField(max_length=500,null=True,verbose_name='Description')
-    user = models.OneToOneField(User,on_delete=models.CASCADE,verbose_name='Create user')
-
+    address = models.CharField(max_length=100,verbose_name='Address',null=True,blank=True)
+    description = models.TextField(max_length=500,verbose_name='Description',null=True,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
     class Meta:
         verbose_name_plural = 'Dealer'
-
 class Salesman(models.Model):
-    name = models.CharField(max_length=100,verbose_name='Name')
-    phone = models.CharField(max_length=12,verbose_name='Phone number')
-    address = models.CharField(max_length=50,verbose_name='Address')
-    email = models.CharField(max_length=100,null=True,verbose_name='E-mail')
+    name = models.CharField(max_length=100,verbose_name='Name',null=True,blank=True)
+    phone = models.CharField(max_length=12,verbose_name='Phone number',null=True,blank=True)
+    address = models.CharField(max_length=50,verbose_name='Address',null=True,blank=True)
+    email = models.CharField(max_length=100,verbose_name='E-mail',null=True,blank=True)
     dealer = models.ForeignKey(Dealer,on_delete=models.CASCADE,related_name='dealer')
-    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='user',null=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='user')
 
     class Meta:
         verbose_name_plural = 'Salesman'
 
 class Item(models.Model):
-        name = models.CharField(max_length=100,null=True,verbose_name='Name')
-        price = models.DecimalField(max_digits=10,decimal_places=3,verbose_name='Price')
-        description = models.TextField(max_length=500,null=True,verbose_name='Description')
+        name = models.CharField(max_length=100,verbose_name='Name',null=True,blank=True)
+        price = models.DecimalField(max_digits=10,decimal_places=3,verbose_name='Price',null=True,blank=True)
+        description = models.TextField(max_length=500,verbose_name='Description',null=True,blank=True)
         attribute = [('In stock','In stock'),('Out of stock','Out of stock')]
-        status = models.CharField(max_length=12,choices=attribute,default='In stock',null=True,verbose_name='Status')
+        status = models.CharField(max_length=12,choices=attribute,default='In stock',verbose_name='Status',null=True,blank=True)
         dealer = models.ForeignKey(Dealer,on_delete=models.CASCADE,related_name='Dealer1')
         class Meta:
             verbose_name_plural = 'Item'
+        
+        def __str__(self):
+            return self.name
 
 class Order(models.Model):
     name = models.CharField(max_length=100,null=True,verbose_name='Name')
@@ -45,5 +49,8 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True,verbose_name='Date Ordered')
     salesman = models.ForeignKey(Salesman,on_delete=models.CASCADE,related_name='salesman_orders',verbose_name='Salesman')
     item = models.ForeignKey(Item,on_delete=models.CASCADE,related_name='item_orders')
+
+    def __str__(self):
+        return self.name
     class Meta:
         verbose_name_plural ='Order'
