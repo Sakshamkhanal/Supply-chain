3d15630a65da25fb42245e65281c4dae2c235475
Saksham <Saksham.khanal01@gmail.com>
2022-03-29 16:45:07 +0545

    With LoginAPI

--- Office/Supply_Chainv1/Suppychain/Suppychain/settings.py
+++ Office/Supply_Chainv1/Suppychain/Suppychain/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Myapp',
+    'dsorders',
     'rest_framework',
+    'rest_framework_api_key',
     'knox',
+
 
 ]
 
@@ -125,7 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'Myapp.User'
+AUTH_USER_MODEL = 'dsorders.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':('knox.auth.TokenAuthentication',)
